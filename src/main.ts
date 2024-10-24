cde18e9cb5e336c292180d3d4ad168da66b5956d
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2024-10-24 17:17:26 +0700

    Added validation pipe

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -17,6 +18,8 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '../src/', 'views'));
   // set view engine (hbs, ejs, pug...)
   app.setViewEngine('ejs');
+  // validation class
+  app.useGlobalPipes(new ValidationPipe())
 
   await app.listen(configService.get<string>("PORT"));
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,13 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 export class CreateUserDto {
+    @IsEmail({}, {message: "Email is invalid"})
+    @IsNotEmpty({message: "Email cannot be empty"})
     email: string;
+
+    @IsNotEmpty({message: "Password cannot be empty"})
     password: string;
+
     name: string;
     address: string;
 }
